fbbea741ffde53f0c37d6c7c7907e0c90a6d84e7
ismail <rixrix461@gmail.com>
2025-08-18 12:31:20 +0100

    Merge branch 'main' of github.com:smorayzmaaitat/ft_ls

---
+++ ft_ls.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pwd.h>
+#include <grp.h>
+#include "libft/libft.h"
+#include "ft_printf/include/ft_printf.h"
+#include <time.h>
+
+typedef struct PathNode {
+    char *path;
+    char *name;
+    time_t mtime_sec;
+    long   mtime_nsec;
+    struct PathNode *next;
+} PathNode;
+
+
+typedef struct {
+    int l;  
+    int a;  
+    int r;  
+    int R;  
+    int t;  
+} t_flags;
